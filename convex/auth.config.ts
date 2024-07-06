fad2c6e5a2ff824fc9a765f3da8e58dabc9ece61
Tommyle38 <lehoangminh9938@gmail.com>
2024-07-06 10:57:31 +0700

    update domain

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
     providers: [
       {
-        domain: "https://relaxing-monkfish-30.clerk.accounts.dev",
+        domain: "poetic-chimp-18.clerk.accounts.dev",
         applicationID: "convex",
       },
     ]
