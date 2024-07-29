d9f0ccd33e27f249ffb6f55be0be710fb0547625
Tommyle38 <lehoangminh9938@gmail.com>
2024-07-29 09:24:10 +0700

    update

---
+++ app/(root)/data-view/page.tsx
@@ -0,0 +1,11 @@
+import DataTable from "@/components/DataTable";
+
+const page = () => {
+  return (
+    <div className="flex w-full justify-center">
+      <DataTable />
+    </div>
+  );
+};
+
+export default page;
